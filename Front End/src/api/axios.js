6ad4c17eb9b5ded192a5b9f4b974e0b7dcbf03dc
mo-elbedewy <moaazelbedawy@gmail.com>
2026-09-01 00:37:41 +0300

    Fix back end

--- Front End/src/api/axios.js
+++ Front End/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: "https://pos-system-backend-orpin.vercel.app",
   withCredentials: true,
 });
 

8a5938579abfd474eacff073d66b4e0a480ee86b
mo-elbedewy <moaazelbedawy@gmail.com>
2026-08-31 23:33:43 +0300

    Fix back end

--- Back End/server.js
+++ Back End/server.js
@@ -22,11 +22,23 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors({
-    origin:["http://localhost:5173" , "http://localhost:5174"],
-    credentials:true
-}))
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://pos-system-commercial.vercel.app",
+  "https://pos-system-admins.vercel.app"
+];
 
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json())
 app.use(cookieParser())
